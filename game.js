a1ad34dc565e3f8647035b114fbe91be67d78706
William46503 <nextlevelpenguin@gmail.com>
2020-02-21 22:31:23 +0900

    Prototype completed 1.0

--- game.js
+++ game.js
@@ -1,45 +1,50 @@
 
 var buttonColors = ["red", "blue", "green", "yellow"];
 var delayTime = 100;
-//System side: Random color pattern//
+//Color pattern//
 var gamePattern = [];
-
-//Player Side: User Clicked Pattern//
 var userClickedPattern = [];
-//Step 1. Create a new pattern//
-$(document).on("click",function nextSequence (){
+
+//GameManager//
+var started = false;
+var level = 0;
+
+function nextSequence (){
+    level++;
+    $("#level-title").text("Level " + level);
   //Random generate Number 1 - 4//
     var randomNumber = Math.floor(Math.random()*4);
-//Step 2. Add new gamePattern//
-  //Choose random color//
     var randomChooseColor = buttonColors[randomNumber];
   //Add random color to gamePattern//
     gamePattern.push(randomChooseColor);
-
 //Step 3. Show the nextSequence by Animation and sound//
-  //Add Flash Animation to new added color//
-    $("#" + randomChooseColor).fadeIn(100).fadeOut(100).fadeIn(100);
-
-  //Play auto according to new chosen color//
+    $("#" + randomChooseColor).fadeOut(150).fadeIn(150);
+  //Play audio according to new chosen color//
     playSound(randomChooseColor);
-});
+}
 
-
-//Step 4(Player Side). Detect clicks on button//
-$(".btn").click(function(){
-  //Detect ID of the clicked button//
-  var userChosenColor = $(this).attr("id");
-
-  //Add to User Clicked Pattern//
-  userClickedPattern.push(userChosenColor);
-
-  //Animate user clicked button//
-  animatePress(userChosenColor);
-
-  //Play Sound according to player clicks//
-  playSound(userChosenColor);
-});
-
+function checkAnswer(currentLevel){
+  //Check if users latest click is equal to gamePatter//
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+    console.log("sucess");
+    if(gamePattern.length === userClickedPattern.length){
+      setTimeout(function(){
+       nextSequence();
+       userClickedPattern = [];
+      },1000);
+    }
+  } else {
+    //Play wrong.mp3, add class to body to flash red//
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    },200);
+    //change H1 to gameover message//
+    $("#level-title").html("Game Over,<br> Press 'A' Key to Restart");
+    gameover();
+  }
+}
 //Step 6: Show user Clicks by adding animation//
 function animatePress(currentColor){
   $("#" + currentColor).addClass("pressed");
@@ -47,7 +52,15 @@ function animatePress(currentColor){
   //Remove Class to create flash effect//
   setTimeout(function () {
   $("#" + currentColor).removeClass("pressed");
-}, delayTime);
+  }, delayTime);
+  }
+
+//game over function to call//
+function gameover(){
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+  started = false;
 }
 
 //Step 5(Both Sides): Add Sound//
@@ -55,3 +68,28 @@ function playSound(name){
   var audio = new Audio("sounds/"+ name +".mp3");
   audio.play();
 }
+
+//When the "A" Key is pressed, call nextSequence, game Start//
+$(document).on("keydown",function(){
+  if (event.keyCode == 65 && started == false){
+    setTimeout(function(){
+      nextSequence();
+      $("#level-title").text("Level " + level);
+      started = true;
+    }, 500);
+  } else if (started == true ){
+    console.log("Already started");
+  }
+});
+
+//Step 4(Player Side). Detect clicks on button//
+$(".btn").click(function(){
+  //Detect ID of the clicked button//
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  //Animate user clicked button//
+  animatePress(userChosenColor);
+  //Play Sound according to player clicks//
+  playSound(userChosenColor);
+  checkAnswer(userClickedPattern.length-1);
+});
